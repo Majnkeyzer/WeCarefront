15845e11149bac9202fafb42eebed9a8e6430e17
Sjoerd Valk <sjoerdvalk@MacBook-Pro-van-Sjoerd.local>
2019-01-03 13:44:57 +0100

    step 1.1 vue included and rendered data

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert('start workshop');
+var app = new Vue({
+    el: '#app',
+    data: {
+      title: 'De parels onder de bieren',
+      beers: [
+            {
+                brewery: "brouwerij Jopen",
+                title: "Mooie Nel I.P.A."
+            },
+            {
+                brewery: "brouwerij Jopen",
+                title: "Malle Babbe"
+            }  
+      ]
+    }
+  });
