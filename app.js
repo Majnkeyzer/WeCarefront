775454af64bc40beee654058407e1fc8f8cc4f73
Sjoerd Valk <sjoerdvalk@mbp-van-sjoerd.home>
2019-01-27 18:10:27 +0100

    step-1.2

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      title: 'De parels onder de bieren',
-      beers: [
-            {
-                brewery: "brouwerij Jopen",
-                title: "Mooie Nel I.P.A."
-            },
-            {
-                brewery: "brouwerij Jopen",
-                title: "Malle Babbe"
-            }  
-      ]
-    }
-  });
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'De parels onder de bieren',
+    beers: [
+      {
+        brewery: 'brouwerij Jopen',
+        title: 'Mooie Nel I.P.A.',
+        percentage: 6.5
+      },
+      {
+        brewery: 'brouwerij Jopen',
+        title: 'Malle Babbe',
+        percentage: 4.5
+      }
+    ]
+  }
+});
