35fc312bd495fbc0b667e59f180a95e8d082b5aa
Sjoerd Valk <sjoerdvalk@192.168.2.45>
2019-02-07 12:20:32 +0100

    step-2.2

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -2,8 +2,17 @@
   <li class="product-item">
     <div class="product-information">
       <header class="product-title">{{ beer.title }}</header>
+      <div>{{ beer.brewery }}</div>
       <div>{{ beer.city }}</div>
       <img class="product-image" :src="beer.img" />
+    </div>
+    <div class="add-container">
+      <button class="btn btn-primary" @click="decrementAmount">-</button>
+      <span>{{ number }}</span>
+      <button class="btn btn-primary" @click="incrementAmount">+</button>
+      <button class="btn btn-primary" @click="handleAddToWishList">
+        Add to wishlist
+      </button>
     </div>
   </li>
 </template>
@@ -11,6 +20,27 @@
 export default {
   props: {
     beer: {}
+  },
+  data() {
+    return {
+      number: 0
+    };
+  },
+  methods: {
+    handleAddToWishList() {
+      this.resetAmount();
+    },
+    resetAmount() {
+      this.number = 0;
+    },
+    decrementAmount() {
+      if (this.number > 0) {
+        this.number--;
+      }
+    },
+    incrementAmount() {
+      this.number++;
+    }
   }
 };
 </script>
